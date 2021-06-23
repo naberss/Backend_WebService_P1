152a8a931dcef929b837b88f69f9936742df4a96
Lucas Moreira <lucas-berto@hotmail.com>
2021-06-23 08:16:51 -0300

    [#1 -> Configurações iniciais do projeto] - Incremento

---
+++ src/main/java/com/nabers/spring/BackendWebServiceP1Application.java
@@ -0,0 +1,13 @@
+package com.nabers.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWebServiceP1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendWebServiceP1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nabers/spring/BackendWebServiceP1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nabers.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWebServiceP1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
