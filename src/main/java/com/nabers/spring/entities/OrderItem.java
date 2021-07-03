827d5fa0b0bb941e22e2f2a9a7d1d8119fc18935
Lucas Moreira <lucas-berto@hotmail.com>
2021-07-03 15:44:14 -0300

    [#10 -> Associação de chave primaria composta] - Incremento
    *Criada tabela OrderItemPK para  servir de chave primaria composta entre a correlação ORDER/PRODUCT.
    *A chave composta foi correlacionada como chave primaria da tabela OrdemItem.

---
+++ src/main/java/com/nabers/spring/entities/OrderItem.java
@@ -0,0 +1,85 @@
+package com.nabers.spring.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import com.nabers.spring.entities.pk.OrderItemPK;
+
+@Entity
+@Table(name = "order_item")
+public class OrderItem implements Serializable {
+
+	@Transient
+	private static final long serialVersionUID = 1L;
+
+	@EmbeddedId
+	private OrderItemPK id;
+	private Integer quantity;
+	private Double Price;
+
+	public OrderItem() {
+	}
+
+	public OrderItem(Product product, Order order, Integer quantity, Double price) {
+		super();
+		id.setProduct(product);
+		id.setOrder(order);
+		this.quantity = quantity;
+		this.Price = price;
+	}
+
+	public Order getOrder() {
+		return id.getOrder();
+	}
+
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+
+	public Product getProduct() {
+		return id.getProduct();
+	}
+
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return Price;
+	}
+
+	public void setPrice(Double price) {
+		Price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
